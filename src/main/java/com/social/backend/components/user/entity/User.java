e34a86b9f9df39e981ed4f96f72e5d7c7b23024e
Cyrus91 <cirodattilo2@gmail.com>
2026-03-20 11:42:21 +0100

    Add default value for email_verified column in User entity

--- src/main/java/com/social/backend/components/user/entity/User.java
+++ src/main/java/com/social/backend/components/user/entity/User.java
@@ -42,7 +42,7 @@ public class User {
     private String avatarUrl;
 
     @Setter
-    @Column(name = "email_verified", nullable = false)
+    @Column(name = "email_verified", nullable = false, columnDefinition = "boolean default false")
     private boolean emailVerified = false;
 
     @Setter
