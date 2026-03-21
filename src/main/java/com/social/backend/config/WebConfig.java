6cb09ff8b78b5259a52f34628c4d138ca5e80843
Cyrus91 <cirodattilo2@gmail.com>
2026-03-21 18:08:58 +0100

    Update CORS configuration to allow requests from the production frontend URL

--- src/main/java/com/social/backend/config/WebConfig.java
+++ src/main/java/com/social/backend/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173","https://social-media-frontend-mu-neon.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
